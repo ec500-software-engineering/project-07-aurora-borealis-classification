c562cfe3e76da9433f8127c664692cd4ae717f72
Xiangkun Ye <xk1997@bu.edu>
2019-03-16 00:27:41 -0400

    API design

---
+++ Feature_Extraction.py
@@ -0,0 +1,31 @@
+'''This file would include APIs used to extract featrues we need from dataset'''
+
+def rotate(path):
+    '''
+    If we need more data than we have, we could rotate them and use them again
+    '''
+
+    pass
+
+def extract(path):
+    '''
+    This function would automatically extract featrues from images in input path
+    '''
+
+    pass
+
+def output(feature):
+    '''
+    Basically it would cooperate with extract function and generate output data
+    in desired type or format.
+    '''
+
+    pass
+
+if __name__ == '__main__':
+    '''
+    When we run this file directly, it will automatically use current path to
+    finish all those jobs.
+    '''
+
+    pass
---
+++ Ridge_Classifier.py
@@ -0,0 +1,28 @@
+'''This file would use the output from Feature_Extraction.py and include APIs of the ridge classifier'''
+
+from Feature_Extraction import rotate
+from Feature_Extraction import extract
+from Feature_Extraction import output
+
+def train(path, modelname):
+    '''
+    This function would automatically train model using features in input path
+    '''
+
+    pass
+
+def predict(modelname, input):
+    '''
+    This function would use selected model and new input to predict and output
+    '''
+
+    dict = {0:"arc", 1:"diffuse", 2:"discrete", 3:"cloudy", 4:"moon", 5:"noaurora"}
+    pass
+
+if __name__ == '__main__':
+    '''
+    When we run this file directly, it will automatically use current path and
+    APIs from Feature_Extraction.py to train a model.
+    '''
+
+    pass
