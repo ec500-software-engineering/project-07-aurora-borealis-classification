447fd0da18a3aaa67d39af2872bdab7a7676b4a0
zhangyuwan <wanzy@bu.edu>
2019-05-08 10:38:49 -0400

    update functions

--- web-flask/app/views.py
+++ web-flask/app/views.py
@@ -4,6 +4,7 @@ import cv2
 from flask import request, url_for
 from flask_login import login_required
 from werkzeug.urls import url_parse
+import subprocess
 
 from app import app
 ALLOWED_EXTENSIONS = set(['png','jpg'])
@@ -49,11 +50,75 @@ def upload_file():
           if not os.path.exists(testpath):
               os.mkdir(testpath)
           cv2.imwrite(os.path.join(testpath,'test.jpg'), img)
-
-          return render_template("uploadsuccess.html",val1=time.time())
+          call_model()
+          y_pred = predition()
+          return render_template("uploadsuccess.html",val1=time.time(),li = y_pred)
       else:
           return "fail"
 
+'''
+    feature extraction
+'''
+def call_model():
+    task = ['python3', '../tensorflow/TF_FeatureExtraction/example_feat_extract.py',
+            '--network', 'inception_v4',
+            '--checkpoint', '../tensorflow/checkpoints/inception_v4.ckpt',
+            '--image_path', './app/static/images/test',
+            '--out_file', './app/static/images/test/auroral_test.h5',
+            '--layer_names', 'Logits']
+    subprocess.call(task)
+
+from sklearn.linear_model import RidgeClassifier
+
+import h5py
+import numpy as np
+import pandas as pd
+
+
+def predition():
+    category ={}
+    category[0] = 'arc --> one or multiple bands of aurora that stretch across the field-of-view'
+    category[1] = 'diffuse --> show large patches of aurora'
+    category[2] = 'discrete --> show auroral forms with well-defined, sharp edges,but not arc like'
+    category[3] = 'cloudy --> The sky in these images is dominated by clouds'
+    category[4] = 'moon --> The image is dominated by light from the Moon'
+    category[5] = 'clear sky --> show a clearsky without the appearance of aurora'
+    base_dir = "../oath/"
+    # read classifications
+    df = pd.read_csv(base_dir + "classifications/classifications.csv", skiprows=18)
+    ndata = len(df["picNum"])
+
+    f = h5py.File(base_dir + "features/auroral_feat.h5", "r")
+    features = f["Logits"].value
+    f.close()
+
+    f_test = h5py.File("./app/static/images/test/auroral_test.h5", "r")
+    features_test = f_test["Logits"].value
+    f_test.close()
+
+    alpha = 0.03
+
+    idxs = np.loadtxt(base_dir + "classifications/train_test_split.csv", delimiter=",").astype(int)
+    cnt = 0
+    for idx in idxs:
+        ntrain = int(np.round(0.7 * ndata))
+        idx_train = idx[0:ntrain]
+
+        X_train = features[idx_train, :]
+        y_train = df["class6"][idx_train]
+
+        clf = RidgeClassifier(random_state=10 * cnt, normalize=False, alpha=alpha)
+        clf.fit(X_train, y_train)
+
+    X_test = features_test
+    y_pred = clf.predict(X_test)
+    return category[y_pred[0]]
+
+
+@app.route('/intro')
+@login_required
+def intro():
+    return render_template("intro.html")
 
 from app import app
 from app.form import LoginForm
@@ -81,6 +146,7 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
 
 
 '''
